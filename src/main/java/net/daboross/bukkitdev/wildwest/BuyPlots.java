3a41d22cd5a99f6f864377cd0d96669156b7a378
Dabo Ross <dabodialga@gmail.com>
2013-07-06 10:29:39 -0700

    Fixed BuyPlots (not sure what it was doing before?)

--- src/main/java/net/daboross/bukkitdev/wildwest/BuyPlots.java
+++ src/main/java/net/daboross/bukkitdev/wildwest/BuyPlots.java
@@ -1,22 +1,25 @@
-import org.bukkit.Bukkit;
+package net.daboross.bukkitdev.wildwest;
+
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
 
+public class BuyPlots implements Listener {
 
-public class Thirst implements Listener{
-  public static Bukkit plugin;
-	
-	public void BukkitListener(Bukkit instance){
-		
-		plugin = instance;
-	}
-     public void onSignCreate(SignChangeEvent sign){
-    	 Player player = sign.getPlayer();
-    	 if(sign.getLine(0).equalsIgnoreCase("[BuyPlots]")){
-    		 player.sendMessage("Your BuyPlots sign has been set up correctly");
-    		 sign.setLine(0, "[BuyPlots]");
-    		 //More to be added, Tired xD
-    	 }
-     }
+    private final WildWestBukkit wildWestBukkit;
+
+    public BuyPlots(WildWestBukkit wildWestBukkit) {
+        this.wildWestBukkit = wildWestBukkit;
+    }
+
+    @EventHandler
+    public void onSignCreate(SignChangeEvent sign) {
+        Player player = sign.getPlayer();
+        if (sign.getLine(0).equalsIgnoreCase("[BuyPlots]")) {
+            player.sendMessage("Your BuyPlots sign has been set up correctly");
+            sign.setLine(0, "[BuyPlots]");
+            //More to be added, Tired xD
+        }
+    }
 }
