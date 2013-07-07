25bf65152baf55d1b065994111b88cc411883b8b
Dabo Ross <dabodialga@gmail.com>
2013-07-07 12:45:56 -0700

    Store fireworkEffect in a variable so that we don't create it each time. Send MessageStatic message instead of local variable. Store wildWestBukkit as reference.

--- src/main/java/net/daboross/bukkitdev/wildwest/JoinListener.java
+++ src/main/java/net/daboross/bukkitdev/wildwest/JoinListener.java
@@ -1,8 +1,5 @@
-/**
- *
- */
 package net.daboross.bukkitdev.wildwest;
- 
+
 import org.bukkit.Color;
 import org.bukkit.FireworkEffect;
 import org.bukkit.Location;
@@ -10,18 +7,27 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
- 
+
+/**
+ *
+ */
 public class JoinListener implements Listener {
-        FireworkEffectPlayer fplayer = new FireworkEffectPlayer();
-        public JoinListener(WildWestBukkit wildWestBukkit) {
- 	}
-        @EventHandler
-        public void onPlayerJoinEvent(PlayerJoinEvent event) throws Exception{
-          if(!event.getPlayer().hasPlayedBefore()) {
-                Player player = event.getPlayer();
-                Location location = player.getLocation();
-            event.getPlayer().sendMessage("Message Here");
-            this.fplayer.playFirework(player.getWorld(), location,FireworkEffect.builder().withColor(Color.GREEN).withFade(Color.GREEN).with(FireworkEffect.Type.BURST).flicker(true).trail(false).build());
-          }
-        }        
+
+    private final WildWestBukkit wildWestBukkit;
+    private final FireworkEffectPlayer fireworkEffectPlayer = new FireworkEffectPlayer();
+    private final FireworkEffect fireworkEffect = FireworkEffect.builder().withColor(Color.GREEN).withFade(Color.GREEN).with(FireworkEffect.Type.BURST).flicker(true).trail(false).build();
+
+    public JoinListener(WildWestBukkit wildWestBukkit) {
+        this.wildWestBukkit = wildWestBukkit;
+    }
+
+    @EventHandler
+    public void onPlayerJoinEvent(PlayerJoinEvent event) throws Exception {
+        if (!event.getPlayer().hasPlayedBefore()) {
+            Player player = event.getPlayer();
+            Location location = player.getLocation();
+            event.getPlayer().sendMessage(MessageStatic.PLAYER_JOIN_WITH_FIREWORK);
+            fireworkEffectPlayer.playFirework(player.getWorld(), location, fireworkEffect);
+        }
+    }
 }
