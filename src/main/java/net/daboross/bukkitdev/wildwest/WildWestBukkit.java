0e87bce65b7ab2c6dd0137f80ec66a46ea5febce
Dabo Ross <daboross@daboross.net>
2013-07-07 12:31:33 -0700

    Fixed file naming.

--- src/main/java/net/daboross/bukkitdev/wildwest/WildWestBukkit.java
+++ src/main/java/net/daboross/bukkitdev/wildwest/WildWestBukkit.java
@@ -38,7 +38,7 @@ public class WildWestBukkit extends JavaPlugin {
         pm.registerEvents(new GoldPanningListener(this), this);
         pm.registerEvents(new MayorMovementListener(this), this);
         pm.registerEvents(new BuyPlotsSignListener(this), this);
-        pm.registerEvents(new JoinListener(this), this)
+        pm.registerEvents(new JoinListener(this), this);
     }
 
     @Override
