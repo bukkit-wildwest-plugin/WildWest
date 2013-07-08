e4156b49d091777045461dbeede529156c5d1bcb
lst96 <trexler96@gmail.com>
2013-07-08 18:48:55 -0400

    Fixed TeleprtingBandits

--- src/main/java/net/daboross/bukkitdev/wildwest/TeleportingBandits.java
+++ src/main/java/net/daboross/bukkitdev/wildwest/TeleportingBandits.java
@@ -3,6 +3,7 @@ package net.daboross.bukkitdev.wildwest;
 import org.bukkit.Material;
 import org.bukkit.block.Sign;
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -13,33 +14,26 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.World;
-import org.bukkit.plugin.java.JavaPlugin;
 
-public class TeleportingBandits extends JavaPlugin implements Listener{
-    @Override
-    public void onEnable() {
-    	Bukkit.getPluginManager().registerEvents(this,this);
-    	getConfig().options().copyDefaults(true);
-        saveConfig();
+public class TeleportingBandits implements Listener, CommandExecutor{
+	private WildWestBukkit plugin;
 
-    }
-    		  
-    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+	  public TeleportingBandits(WildWestBukkit plugin)
+	  {
+	    this.plugin = plugin;
+	  }
+	
+        public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
     	Player p = (Player) sender;
     	if (commandLabel.equalsIgnoreCase("setbandit")){
-            getConfig().set("bandit.world", p.getLocation().getWorld().getName());
-            getConfig().set("bandit.x", p.getLocation().getX());
-            getConfig().set("bandit.y", p.getLocation().getY());
-            getConfig().set("bandit.z", p.getLocation().getZ());
-            saveConfig();
+            plugin.getConfig().set("bandit.world", p.getLocation().getWorld().getName());
+            plugin.getConfig().set("bandit.x", p.getLocation().getX());
+            plugin.getConfig().set("bandit.y", p.getLocation().getY());
+            plugin.getConfig().set("bandit.z", p.getLocation().getZ());
+            plugin.saveConfig();
             p.sendMessage(ChatColor.GREEN + "bandit set!");
-            return true;
-            
-            
-            
-       
-           
-		}
+            return true;                 
+   }
 		return false;
     
     }
@@ -52,19 +46,14 @@ public class TeleportingBandits extends JavaPlugin implements Listener{
             Sign sign = (Sign) p.getClickedBlock().getState();
             if (sign.getLine(0).contains("[Bandit]")) {
             	Player p2 = (Player) p.getPlayer();
-                World w = Bukkit.getServer().getWorld(getConfig().getString("bandit.world"));
-                double x = getConfig().getDouble("bandit.x");
-                double y = getConfig().getDouble("bandit.y");
-                double z = getConfig().getDouble("bandit.z");
+                World w = Bukkit.getServer().getWorld(plugin.getConfig().getString("bandit.world"));
+                double x = plugin.getConfig().getDouble("bandit.x");
+                double y = plugin.getConfig().getDouble("bandit.y");
+                double z = plugin.getConfig().getDouble("bandit.z");
                 p2.teleport(new Location(w, x, y, z));
                 p2.sendMessage(ChatColor.GREEN + "Teleport bandit");
-            }
-        }
-            	
-      }
-
-	@Override
-    public void onDisable() {
-		saveConfig();
-    }
+             }
+         }
+     }  
 }
+
